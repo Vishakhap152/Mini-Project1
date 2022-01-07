0c4bb9da100d580891da3410e2413506228da749
Vishakhap152 <96433618+Vishakhap152@users.noreply.github.com>
2022-01-07 20:47:56 +0530

    First Update

---
+++ ATM.java
@@ -0,0 +1,21 @@
+package com.exmple;
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+      
+	public static void main(String[] args) 
+	{
+		OptionMenu optionMenu =new OptionMenu();
+		
+		try {
+			
+			optionMenu.getLogin();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+
+	}
+
+}
